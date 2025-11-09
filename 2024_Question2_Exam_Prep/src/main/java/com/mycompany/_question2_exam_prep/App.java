edb867778a7463a8e3a7881d14517460b3d09424
kalistareddy1 <ST10459332@vcconnect.edu.za>
2025-11-09 18:26:18 +0200

    Initializing Git Repo

---
+++ 2024_Question2_Exam_Prep/src/main/java/com/mycompany/_question2_exam_prep/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany._question2_exam_prep;
+
+/**
+ *
+ * @author kalis
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
